ec496ab0a1d0bee3640d83c74d258d93f9ea2702
Diego Borigen <borigendiego@gmail.com>
2020-07-24 00:02:11 +1000

    Cleanning commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import OurServices from './components/OurServices'
 
 function App() {
   return (
     <div className="App">
-     {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Gomi
-        </a>
-      </header>*/}
-      <OurServices title={'Gomi section'} />
+        <div>New Gomi site</div>
     </div>
   );
 }
