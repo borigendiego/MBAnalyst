4540c9f24f4c24ff794806118894a02e83c23631
Juan <jborigen9@gmail.com>
2020-07-30 20:24:54 -0300

    Arreglos

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './App.css';
-import Header from "./components/Header/NeoHeader";
+import Header from './components/Header/NeoHeader';
+import Banner from './components/Banner'
 
 function App() {
   return (
-    <div className="App">
-        <Header/>
-        <img src={'https://wallpapercave.com/wp/wp4779329.png'} alt={'fondo'}/>
-        <div>New Gomi site we</div>
+    <div className='App'>
+        <Header />
+        <Banner />
     </div>
   );
 }
 
 export default App;
+
---
+++ src/components/Banner/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../Banner/Banner.css'
+
+const Banner = (props) => {
+    return(
+        <div className={'banner'}>
+            <h2 className={'text'}>Better Data, Better Decisions</h2>
+        </div>
+    )
+};
+
+export default Banner;
