8bfe98b93a58eb05370865d1dea07ecaeb722743
Diego Borigen <borigendiego@gmail.com>
2020-08-28 17:50:55 +1000

    Styles for who we are section - fixes

--- src/components/WhoWeAre/index.js
+++ src/components/WhoWeAre/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import './whoWeAre.css';
-import icono5 from '../assets/images/icono_5.png'
-import icono4 from '../assets/images/icono_4.png'
-import image4 from '../assets/images/image4.jpg'
-import image3 from '../assets/images/image3.jpg'
+//Styles
+import './whoWeAre.scss';
+//Images
+import icono5 from '../assets/images/icono_5.png';
+import icono4 from '../assets/images/icono_4.png';
+import image4 from '../assets/images/image4.jpg';
+import image3 from '../assets/images/image3.jpg';
 
-const WhoWeAre = (props) => {
+const WhoWeAre = () => {
     return (
-        <div className={'whoweare-sections'}>
-            <div className={'whoweare-section-one whoweare-section'}>
+        <div className={'who-we-are-sections'}>
+            <div className={'who-we-are-first-section who-we-are-section'}>
+                <div className={'first-section-overlay'}/>
                 <h1>WHO WE ARE</h1>
                 <p className={'whoweare-text'}>Our mission is to improve businesses thought data analytics
                 projects and produce sustainable tools and resources to decision-makers.
@@ -17,7 +20,7 @@ const WhoWeAre = (props) => {
                 improve service quality through on-time deliveries and customer feedback, there is a customised
                 data analytics solution for you</p>
             </div>
-            <div className={'whoweare-section-two whoweare-section'}>
+            <div className={'section-with-icon who-we-are-section'}>
                     <div className={'whoweare-section-two-logotext'}>
                         <img alt={'logo4'} src={icono4} className={'whoweare-logo-one'}/>
                         <p className={'whoweare-section-paragraph'}>We are creative and analytic minds armed with
@@ -26,7 +29,7 @@ const WhoWeAre = (props) => {
                     </div>
                     <img alt={'imagen4'} src={image4} className={'whoweare-image-one'}/>
             </div>
-            <div className={'whoweare-section-three whoweare-section'}>
+            <div className={'section-with-icon who-we-are-section'}>
                     <img alt={'imagen3'} src={image3} className={'whoweare-image-two'}/>
                     <div className={'whoweare-section-two-logotext'}>
                         <img alt={'logo5'} src={icono5} className={'whoweare-logo-two'}/>
