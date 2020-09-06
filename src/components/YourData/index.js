e3d81aaaeda5b0bad649779f77bb6610f2d105f1
Diego Borigen <borigendiego@gmail.com>
2020-09-06 12:28:18 +1000

    Change favicon and your data css

--- src/components/YourData/index.js
+++ src/components/YourData/index.js
@@ -20,38 +20,53 @@ const YourData = () => {
             <h1 className={'your-data-title'}>YOUR DATA</h1>
             <div className={'your-data-container'}>
                 <div className={'items-wrapper'}>
-                    <div className={`${isTopHover ? 'your-data-items hover-element' : 'your-data-items'}`}>
+                    <div
+                        className={`${isTopHover ? 'your-data-items hover-element' : 'your-data-items'}`}
+                        onMouseEnter={() => setIsTopHover(true)}
+                        onMouseLeave={() => setIsTopHover(false)}
+                    >
                         <img alt={'top-icon'} src={FirstIconOrange} />
                         <p>Define your key business drivers and track them through custom made dashboards.</p>
                     </div>
-                    <div className={`${isLeftHover ? 'your-data-items hover-element' : 'your-data-items'}`}>
+                    <div
+                        className={`${isLeftHover ? 'your-data-items hover-element' : 'your-data-items'}`}
+                        onMouseEnter={() => setIsLeftHover(true)}
+                        onMouseLeave={() => setIsLeftHover(false)}
+                    >
                         <img alt={'left-icon'} src={SecondIconOrange} />
                         <p>Visualise commercial trends to support data driven decisions making.</p>
                     </div>
-                    <div className={`${isBottomHover ? 'your-data-items hover-element' : 'your-data-items'}`}>
+                    <div
+                        className={`${isBottomHover ? 'your-data-items hover-element' : 'your-data-items'}`}
+                        onMouseEnter={() => setIsBottomHover(true)}
+                        onMouseLeave={() => setIsBottomHover(false)}
+                    >
                         <img alt={'bottom-icon'} src={ThirdIconOrange} />
                         <p>Understand what makes your business grow and monitor it at glance.</p>
                     </div>
                 </div>
                 <div className={'your-data-rhombus-container'}>
-                    <div className={'rhombus left'}
-                         onMouseEnter={() => setIsLeftHover(true)}
-                         onMouseLeave={() => setIsLeftHover(false)}
+                    <div
+                        className={`${isLeftHover ? 'rhombus left rhombus-hover' : 'rhombus left'}`}
+                        onMouseEnter={() => setIsLeftHover(true)}
+                        onMouseLeave={() => setIsLeftHover(false)}
                     >
                         <img src={SecondIconWhite} alt={'left-icon'} className={'your-data-icon'}/>
                     </div>
-                    <div className={'rhombus top'}
-                         onMouseEnter={() => setIsTopHover(true)}
-                         onMouseLeave={() => setIsTopHover(false)}
+                    <div
+                        className={`${isTopHover ? 'rhombus top rhombus-hover' : 'rhombus top'}`}
+                        onMouseEnter={() => setIsTopHover(true)}
+                        onMouseLeave={() => setIsTopHover(false)}
                     >
                         <img src={FirstIconWhite} alt={'top-icon'} className={'your-data-icon'}/>
                     </div>
                     <div className={'rhombus right'}>
                         <img src={Image} alt={'right'} className={'your-data-img'} />
                     </div>
-                    <div className={'rhombus bottom'}
-                         onMouseEnter={() => setIsBottomHover(true)}
-                         onMouseLeave={() => setIsBottomHover(false)}
+                    <div
+                        className={`${isBottomHover ? 'rhombus bottom rhombus-hover' : 'rhombus bottom'}`}
+                        onMouseEnter={() => setIsBottomHover(true)}
+                        onMouseLeave={() => setIsBottomHover(false)}
                     >
                         <img src={ThirdIconWhite} alt={'fourth-icon'} className={'your-data-icon'}/>
                     </div>
