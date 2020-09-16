1865952ad4df7355a8f31613453813f0a859f338
Juan <jborigen9@gmail.com>
2020-09-16 15:22:34 -0300

    Who we are image fixed, contact border

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -3,76 +3,81 @@ import './contact.scss';
 
 const Contact = (props) => {
     return(
-        <div className={'contact-section'} id={'contact'}>
-            <div className={'contact-title-section'}>
-                <h1 className={'contact-first-title'}>CONTACT</h1>
-                <h1 className={'contact-second-title'}>AND GET MY QUOTE</h1>
+        <div className={'contact-opacity'}>
+            <div className={'contact-section'} id={'contact'}>
+                <div className={'contact-title-section'}>
+                    <h1 className={'contact-first-title'}>CONTACT</h1>
+                    <h1 className={'contact-second-title'}>AND GET MY QUOTE</h1>
+                </div>
+                <div className={'contact-border'}>
+                    <form className={'contact-form'}>
+                        <section className={'contact-item'}>
+                            <label className={'contact-label'}>Number of dashboards</label>
+                            <input type={'radio'} name={'dashboards'} id={'dashboards'} className={'contact-input-radio'}/>
+                            <label htmlFor={'dashboards'}>1</label>
+                            <input type={'radio'} name={'dashboards'} id={'dashboards'} className={'contact-input-radio'}/>
+                            <label htmlFor={'dashboards'}>2</label>
+                            <input type={'radio'} name={'dashboards'} id={'dashboards'} className={'contact-input-radio'}/>
+                            <label htmlFor={'dashboards'}>3</label>
+                            <input type={'radio'} name={'dashboards'} id={'dashboards'} className={'contact-input-radio'}/>
+                            <label htmlFor={'dashboards'}>4</label>
+                            <input type={'radio'} name={'dashboards'} id={'dashboards'} className={'contact-input-radio'}/>
+                            <label htmlFor={'dashboards'}>5</label>
+                            <input type={'radio'} name={'dashboards'} id={'dashboards'} className={'contact-input-radio'}/>
+                            <label htmlFor={'dashboards'}>More than 5</label>
+                        </section>
+                        <section className={'contact-item'}>
+                            <label className={'contact-label'}>Number of data sources</label>
+                            <input type={'radio'} name={'data-sources'} id={'data-sources'} className={'contact-input-radio'}/>
+                            <label htmlFor={'data-sources'}>Not sure</label>
+                            <input type={'radio'} name={'data-sources'} id={'data-sources'} className={'contact-input-radio'}/>
+                            <label htmlFor={'data-sources'}>1</label>
+                            <input type={'radio'} name={'data-sources'} id={'data-sources'} className={'contact-input-radio'}/>
+                            <label htmlFor={'data-sources'}>2</label>
+                            <input type={'radio'} name={'data-sources'} id={'data-sources'} className={'contact-input-radio'}/>
+                            <label htmlFor={'data-sources'}>3</label>
+                            <input type={'radio'} name={'data-sources'} id={'data-sources'} className={'contact-input-radio'}/>
+                            <label htmlFor={'data-sources'}>More than 3</label>
+                        </section>
+                        <section className={'contact-item'}>
+                            <label className={'contact-label'}>What data sources are you using? ( i.e SQL Server, Web,
+                                Excel, etc.)</label>
+                            <input type={'text'} name={'data-kind'} id={'data-kind'} className={'contact-input'}/>
+                        </section>
+                        <section className={'contact-item'}>
+                            <label className={'contact-label'}>Number of Revision</label>
+                            <input type={'number'} name={'revision'} id={'revision'} className={'contact-input'}/>
+                        </section>
+                        <section className={'contact-item'}>
+                            <label className={'contact-label'}>Expected Timeframe (Weeks)</label>
+                            <input type={'number'} name={'timeframe'} id={'timeframe'} className={'contact-input'}/>
+                        </section>
+                        <section className={'contact-item'}>
+                            <label className={'contact-label'}>Name (required)</label>
+                            <input type={'text'} name={'name'} id={'name'} className={'contact-input'}/>
+                        </section>
+                        <section className={'contact-item'}>
+                            <label className={'contact-label'}>Surname (required)</label>
+                            <input type={'text'} name={'surname'} id={'surname'} className={'contact-input'}/>
+                        </section>
+                        <section className={'contact-item'}>
+                            <label className={'contact-label'}>Mail (required)</label>
+                            <input type={'text'} name={'mail'} id={'mail'} className={'contact-input'}/>
+                        </section>
+                        <section className={'contact-item'}>
+                            <label className={'contact-label'}>Subject</label>
+                            <input type={'text'} name={'subject'} id={'subject'} className={'contact-input'}/>
+                        </section>
+                        <section className={'contact-item'}>
+                            <label className={'contact-label'}>Message</label>
+                            <textarea id={'message'} name={'message'} rows={'5'} cols={'40'} className={'contact-input-box'}/>
+                        </section>
+                        <section className={'contact-item'}>
+                            <input type={'submit'} value={'Send'} className={'contact-input-button'}/>
+                        </section>
+                    </form>
+                </div>
             </div>
-            <form className={'contact-form'}>
-                <section className={'contact-item'}>
-                    <label className={'contact-label'}>Number of dashboards</label>
-                    <input type={'radio'} name={'dashboards'} id={'dashboards'} className={'contact-input'}/>
-                    <label for={'dashboards'}>1</label>
-                    <input type={'radio'} name={'dashboards'} id={'dashboards'} className={'contact-input'}/>
-                    <label for={'dashboards'}>2</label>
-                    <input type={'radio'} name={'dashboards'} id={'dashboards'} className={'contact-input'}/>
-                    <label for={'dashboards'}>3</label>
-                    <input type={'radio'} name={'dashboards'} id={'dashboards'} className={'contact-input'}/>
-                    <label for={'dashboards'}>4</label>
-                    <input type={'radio'} name={'dashboards'} id={'dashboards'} className={'contact-input'}/>
-                    <label for={'dashboards'}>5</label>
-                    <input type={'radio'} name={'dashboards'} id={'dashboards'} className={'contact-input'}/>
-                    <label htmlFor={'dashboards'}>More than 5</label>
-                </section>
-                <section className={'contact-item'}>
-                    <label className={'contact-label'}>Number of data sources</label>
-                    <input type={'radio'} name={'data-sources'} id={'data-sources'} className={'contact-input'}/>
-                    <label for={'data-sources'}>Not sure</label>
-                    <input type={'radio'} name={'data-sources'} id={'data-sources'} className={'contact-input'}/>
-                    <label for={'data-sources'}>1</label>
-                    <input type={'radio'} name={'data-sources'} id={'data-sources'} className={'contact-input'}/>
-                    <label for={'data-sources'}>2</label>
-                    <input type={'radio'} name={'data-sources'} id={'data-sources'} className={'contact-input'}/>
-                    <label for={'data-sources'}>3</label>
-                    <input type={'radio'} name={'data-sources'} id={'data-sources'} className={'contact-input'}/>
-                    <label for={'data-sources'}>More than 3</label>
-                </section>
-                <section className={'contact-item'}>
-                    <label className={'contact-label'}>What data sources are you using? ( i.e SQL Server, Web, Excel, etc.)</label>
-                    <input type={'text'} name={'data-kind'} id={'data-kind'} className={'contact-input'}/>
-                </section>
-                <section className={'contact-item'}>
-                    <label className={'contact-label'}>Number of Revision</label>
-                    <input type={'number'} name={'revision'} id={'revision'} className={'contact-input'}/>
-                </section>
-                <section className={'contact-item'}>
-                    <label className={'contact-label'}>Expected Timeframe (Weeks)</label>
-                    <input type={'number'} name={'timeframe'} id={'timeframe'} className={'contact-input'}/>
-                </section>
-                <section className={'contact-item'}>
-                    <label className={'contact-label'}>Name (required)</label>
-                    <input type={'text'} name={'name'} id={'name'} className={'contact-input'}/>
-                </section>
-                <section className={'contact-item'}>
-                    <label className={'contact-label'}>Surname (required)</label>
-                    <input type={'text'} name={'surname'} id={'surname'} className={'contact-input'}/>
-                </section>
-                <section className={'contact-item'}>
-                    <label className={'contact-label'}>Mail (required)</label>
-                    <input type={'text'} name={'mail'} id={'mail'} className={'contact-input'}/>
-                </section>
-                <section className={'contact-item'}>
-                    <label className={'contact-label'}>Subject</label>
-                    <input type={'text'} name={'subject'} id={'subject'} className={'contact-input'}/>
-                </section>
-                <section className={'contact-item'}>
-                    <label className={'contact-label'}>Message</label>
-                    <textarea id={'message'} name={'message'} rows={'5'} cols={'40'} className={'contact-input'}/>
-                </section>
-                <section className={'contact-item'}>
-                    <input type={'submit'} value={'Send'} className={'contact-input'}/>
-                </section>
-            </form>
         </div>
     )
 };
