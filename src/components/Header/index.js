b5ba138eab4e0996e2f7a7ea960cac90e3d72189
Juan <jborigen9@gmail.com>
2020-10-05 12:08:26 -0300

    Font changes

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,10 +4,10 @@ import '../Header/header.scss'
 const Header = (props) => {
     return (
         <div className='header'>
-            <nav className={'item'}><a href={'#home'}>Home</a></nav>
-            <nav className={'item'}><a href={'#services'}>Services</a></nav>
-            <nav className={'item'}><a href={'#work'}>Work</a></nav>
-            <nav className={'item'}><a href={'#contact'}>Contact</a></nav>
+            <nav className={'header-item'}><a href={'#home'}>Home</a></nav>
+            <nav className={'header-item'}><a href={'#services'}>Services</a></nav>
+            <nav className={'header-item'}><a href={'#work'}>Work</a></nav>
+            <nav className={'header-item'}><a href={'#contact'}>Contact</a></nav>
         </div>
     )
 };
