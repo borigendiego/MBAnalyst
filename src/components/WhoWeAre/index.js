c234ba1571c651fe0705e060dbc418a2e193064a
Diego Borigen <borigendiego@gmail.com>
2020-11-26 16:28:57 +1100

    Add scroll effect on mobile

--- src/components/WhoWeAre/index.js
+++ src/components/WhoWeAre/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import VisibilitySensor from 'react-visibility-sensor';
 //Styles
 import './whoWeAre.scss';
 //Images
@@ -8,6 +9,11 @@ import image4 from '../assets/images/image4.jpg';
 import image3 from '../assets/images/image3.jpg';
 
 const WhoWeAre = () => {
+    const [isElement1Visible, setIsElement1Visible1] = useState(false);
+    const [isElement2Visible, setIsElement2Visible2] = useState(false);
+    const onChangeVisibility1 = (isVisible) => isVisible ? setIsElement1Visible1(true) : setIsElement1Visible1(false);
+    const onChangeVisibility2 = (isVisible) => isVisible ? setIsElement2Visible2(true) : setIsElement2Visible2(false);
+
     return (
         <div className={'who-we-are-sections'}>
             <div className={'who-we-are-first-section who-we-are-section'}>
@@ -18,15 +24,18 @@ const WhoWeAre = () => {
                     combining passion, knowledge and experience to transform the way your business deals with data.
                 </p>
             </div>
-            <div className={'section-with-icon who-we-are-section'}>
+            <VisibilitySensor onChange={onChangeVisibility1}>
+                <div className={`${isElement1Visible ? 'section-with-icon who-we-are-section visibleClass' : 'section-with-icon who-we-are-section'}`}>
                     <div className={'who-we-are-section-two-logo-text'}>
                         <img alt={'logo4'} src={icono4} className={'who-we-are-logo-one'}/>
                         <p className={'who-we-are-section-paragraph'}>We provide end to end BI services tailored
                             to fit your company’s individual needs.</p>
                     </div>
                     <img alt={'image4'} src={image4} className={'who-we-are-image-one'}/>
-            </div>
-            <div className={'section-with-icon who-we-are-section'}>
+                </div>
+            </VisibilitySensor>
+            <VisibilitySensor onChange={onChangeVisibility2}>
+                <div className={`${isElement2Visible ? 'section-with-icon who-we-are-section visibleClass' : 'section-with-icon who-we-are-section'}`}>
                     <img alt={'image3'} src={image3} className={'who-we-are-image-two'}/>
                     <div className={'who-we-are-section-two-logo-text'}>
                         <img alt={'logo5'} src={icono5} className={'who-we-are-logo-two'}/>
@@ -34,7 +43,8 @@ const WhoWeAre = () => {
                             consulting firm based in Melbourne, Australia with extensive experience working
                             remotely in data projects around the world.</p>
                     </div>
-            </div>
+                </div>
+            </VisibilitySensor>
         </div>
     )
 };
