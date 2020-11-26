be3dbd33f01fb615e18ba540797d3bcd18d7348a
Diego Borigen <borigendiego@gmail.com>
2020-11-26 20:41:24 +1100

    Mobile styles on three items

--- src/components/Three/ThreeItem.js
+++ src/components/Three/ThreeItem.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import VisibilitySensor from 'react-visibility-sensor';
 
 const ThreeItem = (props) => {
+    const [isElementVisible, setIsElementVisible] = useState(false);
+    const onChangeVisibility = (isVisible) => isVisible ? setIsElementVisible(true) : setIsElementVisible(false);
+
     return(
-        <div className={'three-logo-container'}>
-            <img alt={'logo'} src={props.image} className={'three-logo'} id={props.id}/>
-            <div className={'three-arrow'} id={props.id} />
-            <p className={'three-text'}>{props.text}</p>
-        </div>
+        <VisibilitySensor onChange={onChangeVisibility}>
+            <div className={`${isElementVisible ? 'three-logo-container visibleClass' : 'three-logo-container'}`}>
+                <img alt={'logo'} src={props.image} className={'three-logo'} id={props.id}/>
+                <div className={'three-arrow'} id={props.id} />
+                <p className={'three-text'}>{props.text}</p>
+            </div>
+        </VisibilitySensor>
     )
 };
 
