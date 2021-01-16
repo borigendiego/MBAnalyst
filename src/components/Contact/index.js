d375c6bd0b5ee362b9e6d1d9b8c579e358aac7af
Diego Borigen <borigendiego@gmail.com>
2021-01-16 14:48:14 +1100

    Add functionality for contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,19 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
 import emailjs from 'emailjs-com';
+//Hook
+import useContactForm from './useContactForm';
 //Styles
 import './contact.scss';
+//Component
+import MyInput from './CustomInput';
+//Fields validations
+import { required, email } from './fieldValidations';
 
 const Contact = () => {
-    console.log('>>process.env', process.env);
-    const onSubmit = (e) => {
-        e.preventDefault();
+    const [messageSent, setMessageSent] = useState('');
+    const initialValues = {
+        name: '',
+        mail: '',
+        subject: '',
+        message: '',
+    };
 
-        emailjs.sendForm(process.env.REACT_APP_MAIL_VALUE, process.env.REACT_APP_TEMPLATE_ID, e.target, process.env.REACT_APP_USER_ID)
-            .then((result) => {
-                console.log(result.text);
-            }, (error) => {
-                console.log(error.text);
-            }); 
+    const {
+        values,
+        handleChange,
+        errors,
+        setErrors,
+        setValues,
+    } = useContactForm({
+        initialValues,
+        onSubmit: values => console.log({ values })
+    });
+
+    const onSubmit = (e, form) => {
+        const { values } = e;
+        const formErrors = {};
+
+        if (!values.name) {
+            formErrors['name'] = 'Required';
+        }
+
+        if (!values.mail) {
+            formErrors['mail'] = 'Required';
+        }
+
+        if (Object.keys(formErrors).length === 0 && formErrors.constructor === Object) {
+            //TODO: Move emails sender to a proper API
+            emailjs.sendForm(process.env.REACT_APP_MAIL_VALUE, process.env.REACT_APP_TEMPLATE_ID, form, process.env.REACT_APP_USER_ID)
+                .then(() => {
+                    setValues(initialValues);
+                    setMessageSent('succeed');
+                }, (error) => {
+                    console.log(error.text);
+                    setMessageSent('error');
+                });
+        } else {
+            setErrors(formErrors);
+        }
+    };
+
+    const handleSubmit = (event) => {
+        if (event) event.preventDefault();
+        onSubmit({ values }, event.target);
+    };
+
+    const renderSentMessage = () => {
+        if (messageSent === 'succeed') {
+            return <div className={'message succeed'}>
+                <h2>Thank you!</h2>
+                <p>Your message has been successfully sent. We will contact you very soon!</p>
+            </div>
+        }
+        if (messageSent === 'error') {
+            return <div className={'message error'}>
+                <h2>Something went wrong</h2>
+                <p>Please try again or send and email to: support@mbanalyst.com</p>
+            </div>
+        }
+        return null;
     };
 
     return(
@@ -23,25 +84,53 @@ const Contact = () => {
                 <h1 className={'contact-second-title'}>AND GET MY QUOTE</h1>
             </div>
             <div className={'contact-border'}>
-                <form className={'contact-form'} onSubmit={onSubmit}>
-                    <section className={'contact-item'}>
-                        <label className={'contact-label'}>Name (required)</label>
-                        <input type={'text'} name={'name'} id={'name'} className={'contact-input'}/>
-                    </section>
-                    <section className={'contact-item'}>
-                        <label className={'contact-label'}>Mail (required)</label>
-                        <input type={'text'} name={'mail'} id={'mail'} className={'contact-input'}/>
-                    </section>
-                    <section className={'contact-item'}>
-                        <label className={'contact-label'}>Subject</label>
-                        <input type={'text'} name={'subject'} id={'subject'} className={'contact-input'}/>
-                    </section>
+                {/*TODO: REFACTOR THIS COMPONENT AND CREATE A GENERIC FORM BUILDER */}
+                <form className={'contact-form'} onSubmit={handleSubmit}>
+                    <MyInput
+                        fieldName={'name'}
+                        fieldType={'text'}
+                        label={'Name (required)'}
+                        handleChange={handleChange}
+                        value={values.name}
+                        validations={[required]}
+                        errors={errors}
+                    />
+                    <MyInput
+                        fieldName={'mail'}
+                        fieldType={'text'}
+                        label={'Mail (required)'}
+                        handleChange={handleChange}
+                        value={values.mail}
+                        validations={[required, email]}
+                        errors={errors}
+                    />
+                    <MyInput
+                        fieldName={'subject'}
+                        fieldType={'text'}
+                        label={'Subject'}
+                        handleChange={handleChange}
+                        value={values.subject}
+                    />
                     <section className={'contact-item'}>
                         <label className={'contact-label'}>Message</label>
-                        <textarea id={'message'} name={'message'} rows={'5'} cols={'40'} className={'contact-input-box'}/>
+                        <textarea
+                            name={'message'}
+                            id={'message'}
+                            onChange={handleChange}
+                            value={values.message}
+                            rows={'5'}
+                            cols={'40'}
+                            className={'contact-input-box'}
+                        />
                     </section>
+                    {renderSentMessage()}
                     <section className={'contact-item'}>
-                        <input type={'submit'} value={'Send'} className={'contact-input-button'}/>
+                        <button
+                            type={'submit'}
+                            className={'contact-input-button'}
+                        >
+                            SUBMIT
+                        </button>
                     </section>
                 </form>
             </div>
