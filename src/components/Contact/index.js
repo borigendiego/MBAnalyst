37965b665bafd043b4bc58f5e832e6384c954eb3
Diego Borigen <diego@iddigital.com.au>
2021-07-03 10:15:53 +1000

    Implement hippo form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,135 +1,49 @@
-import React, { useState } from 'react';
-import emailjs from 'emailjs-com';
-//Hook
-import useContactForm from './useContactForm';
+import React from 'react';
 //Styles
 import './contact.scss';
-//Component
-import MyInput from './CustomInput';
 //Fields validations
 import { required, email } from './fieldValidations';
+import { Form } from 'hippo-components';
 
 const Contact = () => {
-    const [messageSent, setMessageSent] = useState('');
-    const initialValues = {
-        name: '',
-        mail: '',
-        subject: '',
-        message: '',
-    };
-
-    const {
-        values,
-        handleChange,
-        errors,
-        setErrors,
-        setValues,
-    } = useContactForm({
-        initialValues,
-        onSubmit: values => console.log({ values })
-    });
-
-    const onSubmit = (e, form) => {
-        const { values } = e;
-
-        if (!values.name) {
-            setErrors({...errors, 'name': 'Required'});
-        }
-
-        if (!values.mail) {
-            setErrors({...errors, 'mail': 'Required'});
-        }
-
-        if (!errors && (Object.keys(errors).length === 0 && errors.constructor === Object)) {
-            //TODO: Move emails sender to a proper API
-            emailjs.sendForm(process.env.REACT_APP_MAIL_VALUE, process.env.REACT_APP_TEMPLATE_ID, form, process.env.REACT_APP_USER_ID)
-                .then(() => {
-                    setValues(initialValues);
-                    setMessageSent('succeed');
-                }, (error) => {
-                    console.log(error.text);
-                    setMessageSent('error');
-                });
-        }
-    };
-
-    const handleSubmit = (event) => {
-        if (event) event.preventDefault();
-        onSubmit({ values }, event.target);
-    };
-
-    const renderSentMessage = () => {
-        if (messageSent === 'succeed') {
-            return <div className={'message succeed'}>
-                <h2>Thank you!</h2>
-                <p>Your message has been successfully sent. We will contact you very soon!</p>
-            </div>
-        }
-        if (messageSent === 'error') {
-            return <div className={'message error'}>
-                <h2>Something went wrong</h2>
-                <p>Please try again or send and email to: support@mbanalyst.com</p>
-            </div>
-        }
-        return null;
-    };
-
-    return(
+    return (
         <div className={'contact-section'} id={'contact'}>
             <div className={'contact-title-section'}>
                 <h1 className={'contact-first-title'}>CONTACT</h1>
                 <h1 className={'contact-second-title'}>AND GET MY QUOTE</h1>
             </div>
             <div className={'contact-border'}>
-                {/*TODO: REFACTOR THIS COMPONENT AND CREATE A GENERIC FORM BUILDER */}
-                <form className={'contact-form'} onSubmit={handleSubmit}>
-                    <MyInput
-                        fieldName={'name'}
-                        fieldType={'text'}
-                        label={'Name (required)'}
-                        handleChange={handleChange}
-                        value={values.name}
-                        validations={[required]}
-                        errors={errors}
-                    />
-                    <MyInput
-                        fieldName={'mail'}
-                        fieldType={'text'}
-                        label={'Mail (required)'}
-                        handleChange={handleChange}
-                        value={values.mail}
-                        validations={[required, email]}
-                        errors={errors}
-                    />
-                    <MyInput
-                        fieldName={'subject'}
-                        fieldType={'text'}
-                        label={'Subject'}
-                        handleChange={handleChange}
-                        value={values.subject}
-                    />
-                    <section className={'contact-item'}>
-                        <label className={'contact-label'}>Message</label>
-                        <textarea
-                            name={'message'}
-                            id={'message'}
-                            onChange={handleChange}
-                            value={values.message}
-                            rows={'5'}
-                            cols={'40'}
-                            className={'contact-input-box'}
-                        />
-                    </section>
-                    {renderSentMessage()}
-                    <section className={'contact-item'}>
-                        <button
-                            type={'submit'}
-                            className={'contact-input-button'}
-                        >
-                            SUBMIT
-                        </button>
-                    </section>
-                </form>
+                <Form
+                    fields={[
+                        {
+                            name: 'name',
+                            type: 'text',
+                            label: 'Name (Required)',
+                            validations: [required],
+                        },
+                        {
+                            name: 'customerEmail',
+                            type: 'text',
+                            label: 'Email (Required)',
+                            validations: [email],
+                        },
+                        {
+                            name: 'subject',
+                            type: 'text',
+                            label: 'Subject',
+                        },
+                        {
+                            name: 'message',
+                            type: 'textArea',
+                            label: 'Message',
+                        }
+                    ]}
+                    onSuccessMessage={'Your message has been successfully sent. We will contact you very soon!'}
+                    onErrorMessage={'Please try again in some minutes'}
+                    submitButtonLabel={'SUBMIT'}
+                    customClass={'custom-form-class'}
+                    emailServiceURL={'https://thehippoapi.netlify.app/.netlify/functions/api/mbanalyst-mail'}
+                />
             </div>
         </div>
     )
