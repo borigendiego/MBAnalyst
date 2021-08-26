7e594eafaec42dc5ba343002dc145df7c9766210
Diego Borigen <diego@iddigital.com.au>
2021-08-27 08:30:35 +1000

    Add code dynamic loading

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 const withOptimizedImages = require('next-optimized-images');
+const withPlugins = require('next-compose-plugins');
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+})
 
-module.exports = withOptimizedImages({
+module.exports = withPlugins(
+    [withOptimizedImages, withBundleAnalyzer],
+    {
     webpack(config, se) {
         if (!se.isServer) {
             config.node = {
